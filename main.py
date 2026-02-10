b502ace39042cea6dfa967ea7ac51a56c5776962
Pratyaksh <pratyakshbasel@gmail.com>
2026-02-10 22:55:33 +1100

    add screen recording and using a  better calling syntax

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from dotenv import load_dotenv;
 import datetime;
 import subprocess
 import os;
+import ffmpeg
 
 # load env file
 load_dotenv()
 
 API_TOKEN = os.getenv("API_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+FFMPEG_PATH = "/opt/homebrew/bin/ffmpeg" 
 
 
 if(API_TOKEN):
@@ -35,22 +37,10 @@ def show_macos_popup(message_text):
     except Exception as e:
         print(f"Unexpected error in show_macos_popup: {e}", flush=True)
         return False
-
-    
-    
-# testing for a healthy ping
-@bot.message_handler(commands=['ping'])
-def handle_ping(message):
-    if(message.chat.id == authorisation_id):
-        bot.reply_to(message, "pong!")
         
 # screenshot for the active screen        
-@bot.message_handler(commands=['ss', 'screenshot', 'screen'])
-def handle_screenShot(message):
-    # veryfication
-    if(message.chat.id != authorisation_id):
-        return 
-    
+def take_screenshot_and_send(message):
+        
     # create a temporary image path
     timestamp = datetime.datetime.now().strftime("%d-%m-%Y")
     path = f"/tmp/screenshot_{timestamp}.png"    
@@ -58,8 +48,7 @@ def handle_screenShot(message):
     
     try:
         # run subprocess
-        subprocess.run(["screencapture", "-x", "-D", "1", path], check=True)
-        
+        subprocess.run(["screencapture", "-x", "-D", "1", path], check=True)        
         # send picture to chat
         with open(path, "rb") as photo:
             bot.send_photo(message.chat.id, photo, caption=f"Screenshot at {timestamp}")
@@ -67,10 +56,105 @@ def handle_screenShot(message):
     except Exception as e:
         print(f"Error taking screenshot: {e}")
         bot.reply_to(message, f"Error: {e}")
+        return False
             
     finally:  # delete file once sent
         if(os.path.exists(path)):    
             os.remove(path)
+            
+    return True
+
+
+# function to take a screen recording for a set duration of time
+def take_screenrecording_and_send(chat_id, duration) -> bool:
+    timestamp = datetime.datetime.now().strftime("%d-%m-%Y")
+    output_dir = "/tmp/screenrecordings"
+    raw_path = f"{output_dir}/{timestamp}.mov"
+    final_path = f"{output_dir}/{timestamp}.mp4"
+    screencapture_cmd = ["screencapture", "-V", str(duration), "-x", "-k", raw_path]
+    
+    os.makedirs(output_dir, exist_ok=True)
+    
+    try:
+        bot.send_message(chat_id, "starting recording")
+        print("executing screencapture command", flush=True)
+        subprocess.run(screencapture_cmd, check=True, capture_output=True)
+        
+        if(not os.path.exists(raw_path) or os.path.getsize(raw_path) == 0):
+            print("error capturing the video file doesn't exist or size is 0.")
+            return False
+        
+        print("recording capture successful. Converting to mp4 usinf ffmpeg")
+        ffmpeg_cmd = [
+            FFMPEG_PATH, "-i", raw_path, 
+            "-c:v", "libx264", 
+            "-preset", "ultrafast", 
+            "-pix_fmt", "yuv420p", 
+            "-crf", "23", 
+            "-movflags", "+faststart", 
+            final_path
+        ]
+        subprocess.run(ffmpeg_cmd, check=True, capture_output=True, text=True)
+
+        if not os.path.exists(final_path) or os.path.getsize(final_path) == 0:
+            print("error: MP4 file doesn't exist or is empty after conversion")
+            return False
+        
+        print(f"conversion to mp4 successful sending to telegram")
+        
+        with open(final_path, "rb") as video:
+            bot.send_video(chat_id, video, caption=f"screen recording taken on {timestamp}. duration: {duration}")
+            
+    except subprocess.SubprocessError as err:
+        print("subprocess error", err)
+        return False
+    finally:
+        os.remove(raw_path)
+        os.remove(final_path)
+        
+    return True
+        
+    
+       
+@bot.message_handler(commands=['ss', 'screenshot', 'screen'])
+def handle_screenshot(message):
+    if(message.chat.id != authorisation_id):
+        bot.reply_to(message, "you are not authorised by the admin")
+        return
+    if not take_screenshot_and_send(message):
+        bot.reply_to(message, "error taking a screenshot")
+        
+        
+        
+@bot.message_handler(commands=['screenrec', 'screenrecord', 'sr'])
+def handle_screenrecording(message):
+    
+    # authorise
+    if(message.chat.id != authorisation_id):
+        bot.reply_to(message, "Not authorised by admin")
+    
+    # check for 2 arguments
+    args = message.text.split()    
+    if(len(args) < 2):
+        bot.reply_to(message, f"Usage: /{message} <duration>\n Example: /{message} 15")
+    
+    # send to related function
+    try:
+        duration = int(args[1])
+        if not (5 <= duration <= 300):
+            bot.reply_to(message, "duration has to be between 5 and 300 seconds (5 min).")
+            
+        if not (take_screenrecording_and_send(message.chat.id, duration)):
+            bot.reply_to(message, "recording failed")
+            
+    # check for duration as a number
+    except ValueError:
+        bot.reply_to(message, "duration has to be a number between 5 and 300")
+    except Exception:
+        bot.reply_to(message, "error during recording please refre to console")
+                    
+        
+        
         
         
 if __name__ == "__main__":
